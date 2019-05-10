e903f9dcf3bf946db424e746690b761934b37b84
linhnt <nhokkuteo1996@gmail.com>
2019-05-10 11:10:51 +0700

    update html, css, js

---
+++ js/home.js
@@ -0,0 +1,12 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 72 || document.documentElement.scrollTop > 72) {
+        document.getElementById("navbar").style.padding = "10px 0";
+        document.getElementById("logo").style.width = "150px";
+    } else {
+        document.getElementById("navbar").style.padding = "10px 0";
+        document.getElementById("logo").style.width = "180px";
+    }
+}
