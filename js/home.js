622ea4dd3e2b5b623be28c51253a15a704b5ef0d
linhnt <nhokkuteo1996@gmail.com>
2019-05-10 19:36:07 +0700

    Update html, css, js

--- js/home.js
+++ js/home.js
@@ -17,3 +17,22 @@ $(document).ready(function(){
         $('#menu').slideToggle(200);
     });
 });
+
+//sticky menu
+function sticktothetop() {
+    var window_top = $(window).scrollTop();
+    var top = $('#stick-here').offset().top;
+    if (window_top > top ) {
+        $('#stickThis').addClass('stick');
+        $('#stick-here').height($('#stickThis').outerHeight());
+    } 
+    else {
+        $('#stickThis').removeClass('stick');
+        $('#stick-here').height(0);
+    }
+}
+$(function() {
+    $(window).scroll(sticktothetop);
+    sticktothetop();
+});
+// End Sticky
